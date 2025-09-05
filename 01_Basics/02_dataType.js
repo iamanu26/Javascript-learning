626d1b18e7bb20152d141aa1ac89cf5f65cb4701
Anurag Dubey <anuragdubey26202@gmail.com>
2025-09-06 00:11:45 +0530

    Add files via upload

---
+++ 01_Basics/02_dataType.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3)   // we are using it in nodejs not in browser, now in newer version we have new way to write it 
+
+// always try to mentain the readibility of code; 
+// Documentation 1) tc39 , 2)mdn;
+
+let name = "Anurag" ;
+let age = 25;
+let logedIn = true;
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value   or  it is also explain as 
+// undefined => currently data is not assigned;
+// symbol => unique
+
+
+// Object
+
+console.log(typeof "anurag");
