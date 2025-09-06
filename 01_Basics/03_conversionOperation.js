d3f713f21eb5d042facce84c5d5c7731e7d49080
Anurag Dubey <anuragdubey26202@gmail.com>
2025-09-06 19:32:17 +0530

    Add files via upload

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,13 @@
+let score1 = "33";
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+
+// "33" => convert to number === easily
+//"33abc" => output is NaN. why? --> because this string is not convertable to integer;
+// true => 1 ,,, false =>0;
+// *****************************OPERATIONS IN JAVA SCRIPTS ***************************************
+
+//console.log(2*2);
+
