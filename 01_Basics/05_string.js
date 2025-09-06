775d6ce9e57a2afa8baf1acc234e9fd7f99ed737
Anurag Dubey <anuragdubey26202@gmail.com>
2025-09-06 19:33:31 +0530

    Add files via upload

---
+++ 01_Basics/05_string.js
@@ -0,0 +1,15 @@
+const name = "Anurag";
+const repoCount = "500";
+//console.log(name + repoCount); ths method is outdated, so use below given method;
+
+console.log(`my name is ${name} and my repo count is ${repoCount}`);
+
+const game = new String(`anurag`)
+console.log(name.indexOf(`a`));
+console.log(name.slice(0,4));
+
+const space = "kjsnvkj kjnkjnkjv      ekjrnrkjrnv"
+console.log(space.trim());
+
+
+
