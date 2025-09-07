fb1294020a3dc4d84c9454c5be5495ffe90fcba8
Anurag Dubey <anuragdubey26202@gmail.com>
2025-09-07 22:48:16 +0530

    Add files via upload

---
+++ 01_Basics/07_dates.js
@@ -0,0 +1,8 @@
+//Dates
+
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(typeof(myDate))
+
+let myCreatedDate = new Date(2023 , 0 , 12)
+console.log(myCreatedDate.toString())
