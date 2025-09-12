e6d5b248d1bba3210ab839a07f65ce58ef7a8ae4
Anurag Dubey <anuragdubey26202@gmail.com>
2025-09-12 18:48:01 +0530

    Add files via upload

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,61 @@
+//if
+
+const isUserloggedIn = true
+/* 
+    > greater then
+    < less than
+    >= greater then equal to
+    <= less then equal to
+    = assignment operator
+    == equal to (do not chect type of eg if(2 =="2") it will return true becuse it will not check the type element)
+    === equal to (it check the type of element)
+    !==
+*/
+
+/*
+if(isUserloggedIn)
+{
+    console.log("executed");
+}
+    */
+
+// const temp = 41
+
+// if(temp<50){
+
+//     console.log("correct");
+    
+// }else{
+
+//     console.log("wrong");
+    
+// }
+
+const balance = 1000;
+//if(balance>500) console.log("test") , console.log("test2"); // this is correct but not recomended due the readibility issue;
+
+//else  if
+
+// if(balnce<500)
+// {
+//     console.log("test1");
+    
+// }
+// else if(balance<750)
+// {
+//     console.log("teste2");
+    
+// }
+// else{
+//     console.log("not pass");
+    
+// }
+
+const  userLoggedin = true;
+const debitcard = true;
+
+if(userLoggedin && debitcard){
+    console.log("pass");
+    
+}
+
